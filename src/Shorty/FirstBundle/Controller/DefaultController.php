0a48539b6c07f18582fc411d1647c3078a800fa7
GuGarcia <garciaguillaume69@gmail.com>
2014-03-10 16:53:10 +0100

    # Modify .gitignore + Add Bootstrap

--- src/Shorty/FirstBundle/Controller/DefaultController.php
+++ src/Shorty/FirstBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Shorty\FirstBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -11,4 +12,29 @@ class DefaultController extends Controller
         $view = 'ShortyFirstBundle:Default:index.html.twig';
         return $this->render($view);
     }
+
+    public function addAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add("lien", "text")
+            ->add("Créer", "submit")
+            ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isValid()) {
+            $lien = $request->get("form")["lien"];
+        } else {
+            $lien = null;
+        }
+
+        return $this->render(
+            "ShortyFirstBundle:Default:editer.html.twig",
+            array(
+                "title" => "Ajout d'un Lien",
+                "form" => $form->createView(),
+                "lien" => $lien
+            )
+        );
+    }
+
 }
