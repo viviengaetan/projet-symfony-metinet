4e020b597992cae9aee7854b063e3e5b2b184fbb
GuGarcia <garciaguillaume69@gmail.com>
2014-03-13 11:13:23 +0100

    # Version 0.3.1 + TinyMce, jQuery, jQuery-ui + Admin Category

--- src/GGTeam/ForumBundle/Repository/CategoryRepository.php
+++ src/GGTeam/ForumBundle/Repository/CategoryRepository.php
@@ -8,7 +8,6 @@
 
 namespace GGTeam\ForumBundle\Repository;
 
-
 use Doctrine\ORM\EntityRepository;
 
 class CategoryRepository extends EntityRepository{
@@ -36,4 +35,33 @@ class CategoryRepository extends EntityRepository{
             ->where("c.parent = :parentId")
             ->setParameter("parentId", $parentId);
     }
+
+    public function getParentPossible($id) {
+        $allDeniedCategories = $this->getAllChild($id);
+        $request = $this->_em->createQueryBuilder()
+            ->select("c")
+            ->from("GGTeamForumBundle:Category", "c")
+            ->where("c.id != :id")
+            ->orWhere("c.parent is null");
+        foreach($allDeniedCategories as $cat) {
+            $request->andWhere("c.id != :id".$cat->getId());
+            $request->setParameter("id".$cat->getId(), $cat->getId());
+
+        }
+        $request->setParameter("id", $id);
+        return $request;
+    }
+
+    private function getAllChild($id) {
+        $categories = $this->getSubCategories($id)->getQuery()->getResult();
+        $allcategories = $categories;
+        foreach($categories as $category) {
+            $retour = $this->getAllChild($category->getId());
+            foreach($retour as $cat) {
+                $allcategories[] = $cat;
+            }
+        }
+        return $allcategories;
+    }
+
 } 
